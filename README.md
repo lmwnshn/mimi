18120ea96dabc0456ba42eccea1298389878678e
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2017-03-14 18:09:28 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # mimi
-Loads youtube links for a last.fm user's recent tracks into VLC.
+Continually loads [YouTube](https://www.youtube.com/) links for a [Last.fm](https://www.last.fm/) user's recent tracks into [VLC](http://www.videolan.org/vlc/).
+
+## Details
+
+For when you want to listen "live" to someone's scrobbles.
+
+Why not the Last.fm built-in player? 
+
+- The built-in player relies on people adding a YouTube video for each track. That's a non-trivial amount of work, especially when more than half of the track list is missing videos.
+
+- Furthermore, Last.fm RSS feeds have been broken since [September 2015](https://twitter.com/lastfm/status/644113840855080961). To listen "live", you would need to make something anyway.
+
+So this script calls the Last.fm API to get a user's recent tracks, and then for each track:
+
+1. If Last.fm already has a YouTube video, load it into VLC
+2. Otherwise call the YouTube Data API and load the first video result into VLC
+
+The script repeats the above every X seconds, where X is determined by whether the user was recently active. (refreshing more often if they were, and less often otherwise)
+
+## Usage
+**Configuration:**  
+1. VLC needs to have its [rc](https://wiki.videolan.org/documentation:modules/rc/) interface configured, preferably as an extra interface.
+2. Fill in `config.json` with: host and port for VLC's rc interface, Last.fm user and API key, (optional but recommended) YouTube API key
+
+**Once configured:**  
+1. Start VLC.
+2. `python mimi.py`
+
+## Notes
+- Currently, VLC 2.2.4 youtube.luac fails to extract the video URL for certain YouTube videos, resulting in a "VLC is unable to open the MRL ..." error. It is possible to just supply the video URL directly by using [pafy](https://pypi.python.org/pypi/pafy), but this results in an unreadable playlist. It would be cleaner to fix VLC than to band-aid around it here.
---
+++ mimi.py
@@ -0,0 +1,232 @@
+"""
+Gets recent tracks from last.fm and loads their youtube links into VLC.
+"""
+
+import datetime
+import json
+import telnetlib
+import time
+import lxml.html
+
+import requests
+
+
+class VLCrc:
+    """
+    Connect to VLC via its rc interface through telnet.
+    """
+
+    def __init__(self, server, port):
+        self.server = server
+        self.port = port
+        self.telnet = telnetlib.Telnet(self.server, self.port)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.telnet.close()
+        self.telnet = None
+
+    def enqueue(self, youtube_url):
+        """
+        Adds youtube_url to VLC's playlist.
+        """
+        message = 'enqueue {url}\n'.format(url=youtube_url)
+        self.telnet.write(message.encode('ascii'))
+
+
+class Scrobbler:
+    """
+    Connect to last.fm audioscrobbler API.
+    """
+
+    def __init__(self, user, lastfm_key):
+        self.user = user
+        self.lastfm_key = lastfm_key
+
+    def get_tracks(self, last_ts, limit=25):
+        """
+        Return up to limit (defaul 20) tracks since last_ts (UNIX time).
+        """
+        recenttracks = {
+            'method': 'user.getrecenttracks',
+            'user': self.user,
+            'api_key': self.lastfm_key,
+            'format': 'json',
+            'from': last_ts,
+            'limit': limit
+        }
+
+        scrobblerapi2 = r'http://ws.audioscrobbler.com/2.0/'
+        req = requests.post(scrobblerapi2, params=recenttracks)
+        tracks = req.json()['recenttracks']['track']
+        return tracks
+
+
+class YoutubeLinker:
+    """
+    Obtain youtube video links.
+    """
+
+    def __init__(self, youtube_key=None):
+        self.youtube_key = youtube_key
+
+    def _get_from_lastfm(self, track):
+        lastfm_url = track['url']
+        req = requests.get(lastfm_url)
+
+        tree = lxml.html.fromstring(req.content)
+        links = tree.xpath('(//@data-youtube-url)[last()]')
+
+        if len(links) == 0:
+            return None
+        else:
+            return links[0]
+
+    def _get_from_youtube(self, track):
+        if self.youtube_key is None:
+            return None
+
+        track_name = track['name']
+        track_author = track['artist']['#text']
+
+        max_results = 5
+        # youtube is usually good about returning relevant results
+        # within the top 5, if it exists
+
+        query = '{name} {author}'.format(name=track_name, author=track_author)
+
+        params = {
+            'part': 'snippet',
+            'q': query,
+            'key': self.youtube_key,
+            'maxResults': max_results
+        }
+
+        ytsearchapi3 = r'https://www.googleapis.com/youtube/v3/search'
+        req = requests.get(ytsearchapi3, params)
+
+        video_url = r'https://www.youtube.com/watch?v={video_id}'
+
+        i = 0
+        while i < max_results:
+            ith_result = req.json()['items'][i]['id']
+            if 'videoId' in ith_result:
+                video_id = ith_result['videoId']
+                video_url = video_url.format(video_id=video_id)
+                break
+            else:
+                i = i + 1
+        else:
+            video_url = None
+
+        return video_url
+
+    def get_youtube_url(self, track):
+        """
+        Return youtube_url for a last.fm track object. May return None.
+
+        It first tries last.fm to see if there is a youtube video already,
+        otherwise it makes a search API call to Youtube.
+        """
+        url = self._get_from_lastfm(track)
+        url = self._get_from_youtube(track) if url is None else url
+
+        return url
+
+
+def load_tracks(tracks, last_ts, vlcrc, youtube_linker, verbose=False):
+    """
+    Loads the youtube URL of every track into VLC,
+    returning UNIX time of the latest track.
+    """
+    if len(tracks) == 0:
+        return last_ts
+
+    # we add one to prevent obtaining the same last video again
+    last_ts = int(tracks[0]['date']['uts']) + 1
+
+    # we reverse tracks here to have our playlist
+    # match the last.fm play order, playing towards the present
+    for track in reversed(tracks):
+        youtube_url = youtube_linker.get_youtube_url(track)
+        if youtube_url is None:
+            continue
+
+        vlcrc.enqueue(youtube_url)
+
+        if verbose:
+            track_name = track['name']
+            video_id = youtube_url[youtube_url.find('?v=')+3:]
+            print('[{id}] Loaded {name}'.format(name=track_name, id=video_id))
+
+    return last_ts
+
+
+def load_config():
+    """
+    Return contents of local file config.json as a dictionary.
+
+    Expected from config.json:
+    host: the server on which VLC's rc interface is running 
+          (note that the rc interface is insecure and should not be exposed,
+           it is recommended that host is always 127.0.0.1,
+           this option is here so you can shoot yourself in the foot
+           if you want to)
+    port: the port on which VLC's rc interface is running
+    lastfm_user: the person whose last.fm we are following
+    lastfm_key: api key for last.fm
+    youtube_key: api key for Youtube Data API
+    active_window_min: window in minutes for user recently active
+    awake_min: time in minutes before we poll last.fm for update (fast)
+    asleep_min: time in minutes before we poll last.fm for update (slow)
+    """
+    with open('config.json', 'r') as jsonfile:
+        config = json.load(jsonfile)
+
+    return config
+
+
+def main():
+    """
+    Magic.
+    """
+    last_ts = 0
+
+    config = load_config()
+    host = config['host']
+    port = config['port']
+    lfm_user = config['lastfm_user']
+    lfm_key = config['lastfm_key']
+    yt_key = config['youtube_key']
+    active_window_s = 60 * int(config['active_window_min'])
+    awake_s = 60 * int(config['awake_min'])
+    asleep_s = 60 * int(config['asleep_min'])
+
+    scrobbler = Scrobbler(lfm_user, lfm_key)
+    linker = YoutubeLinker(yt_key)
+
+    while True:
+        with VLCrc(host, port) as vlcrc:
+            tracks = scrobbler.get_tracks(last_ts)
+            last_ts = load_tracks(tracks, last_ts, vlcrc,
+                                  linker, verbose=True)
+
+            now = datetime.datetime.now()
+            print(str(now), "| last_ts:", last_ts)
+
+            recently_active = now.timestamp() - last_ts < active_window_s
+
+            if recently_active:
+                wake_time = now + datetime.timedelta(seconds=awake_s)
+                print("[RECENTLY ACTIVE] sleeping until", str(wake_time))
+                time.sleep(awake_s)
+            else:
+                wake_time = now + datetime.timedelta(seconds=asleep_s)
+                print("[NOT ACTIVE] sleeping until", str(wake_time))
+                time.sleep(asleep_s)
+
+
+if __name__ == "__main__":
+    main()
